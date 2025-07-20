289f7b5b1874a3c4ccbdd9f634cec2fa4c69efdf
oeoott <asdlatnwjd@gmail.com>
2025-07-21 01:09:31 +0900

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-// src/App.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import Login from './pages/Login';
+import Signin from './pages/Signin';
+import Success from './pages/Success';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('login');
+
   return (
-    <div className="app-container">
-      <Login />
-    </div>
+    <>
+      {currentPage === 'login' && <Login setPage={setCurrentPage} />}
+      {currentPage === 'signin' && <Signin setCurrentPage={setCurrentPage} />}
+      {currentPage === 'success' && <Success setCurrentPage={setCurrentPage} />}
+    </>
   );
 }
 
